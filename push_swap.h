b6d72483683d55fef826eefec6d16911184ea2ae
tamagoyakii <rlawlgus4063@naver.com>
2022-07-26 22:11:22 +0900

    push_swap

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jihyukim <jihyukim@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 14:58:55 by jihyukim          #+#    #+#             */
-/*   Updated: 2022/07/26 22:08:55 by jihyukim         ###   ########.fr       */
+/*   Updated: 2022/07/26 22:11:15 by jihyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,21 @@ typedef struct s_deque
 	int		size;
 }				t_dq;
 
+void	push_swap(t_dq *a, t_dq *b);
+void	set_index(t_dq *a);
+void	parsing(char **argv, t_dq *a);
+
+int		get_max_pos(t_dq *b, int max_idx);
+void	under_five(t_dq *a, t_dq *b);
+void	atob(t_dq *a, t_dq *b);
+void	btoa(t_dq *a, t_dq *b);
+
+void	operate_and_print(int op, char *c, t_dq *dst, t_dq *src);
+void	swap(t_dq *dq);
+void	push(t_dq *dst, t_dq *src);
+void	rotate(t_dq *dq);
+void	reverse_rotate(t_dq *dq);
+
 void	error_exit(char *str);
 int		cal_chunk(int x);
 int		is_sorted(t_dq *a);
@@ -44,20 +59,5 @@ void	free_arr(char **arr);
 void	init_dq(t_dq *dq);
 void	add_node(t_dq *a, int num);
 void	free_node(t_dq *dq);
-
-void	operate_and_print(int op, char *c, t_dq *dst, t_dq *src);
-void	swap(t_dq *dq);
-void	push(t_dq *dst, t_dq *src);
-void	rotate(t_dq *dq);
-void	reverse_rotate(t_dq *dq);
-
-int		get_max_pos(t_dq *b, int max_idx);
-void	under_five(t_dq *a, t_dq *b);
-void	atob(t_dq *a, t_dq *b);
-void	btoa(t_dq *a, t_dq *b);
-
-void	push_swap(t_dq *a, t_dq *b);
-void	set_index(t_dq *a);
-void	parsing(char **argv, t_dq *a);
 
 #endif
