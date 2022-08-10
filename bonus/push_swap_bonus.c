423a756e4335247ec54e0d0114a2ceb0ffde2bb9
tamagoyakii <rlawlgus4063@naver.com>
2022-08-10 16:50:11 +0900

    push_swap

--- bonus/push_swap_bonus.c
+++ bonus/push_swap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jihyukim <jihyukim@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 15:32:43 by jihyukim          #+#    #+#             */
-/*   Updated: 2022/08/08 18:48:37 by jihyukim         ###   ########.fr       */
+/*   Updated: 2022/08/10 16:49:57 by jihyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,29 @@ void	push_swap_bonus(char *op, t_dq *a, t_dq *b)
 		operate_bonus(B_RRR, a, b);
 	else
 		error_exit("");
+}
+
+void	set_index(t_dq *a)
+{
+	t_node	*i;
+	t_node	*j;
+
+	i = a->head;
+	while (i)
+	{
+		j = i->next;
+		while (j)
+		{
+			if (i->content > j->content)
+				i->idx += 1;
+			else if (i->content < j->content)
+				j->idx += 1;
+			else
+				error_exit("Error\n");
+			j = j->next;
+		}
+		i = i->next;
+	}
 }
 
 void	parsing(char **argv, t_dq *a)
@@ -79,6 +102,7 @@ int	main(int argc, char **argv)
 	init_dq(&a);
 	init_dq(&b);
 	parsing(argv, &a);
+	set_index(&a);
 	check_op(&a, &b);
 	check_sort(&a, &b);
 	return (0);
